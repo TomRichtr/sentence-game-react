308533bb36fa21f4f70c3f26f2aa0780dbf38e29
TomasR <t.richtr@email.cz>
2020-08-10 12:13:09 +0200

    x

--- src/components/AddInputsPage.tsx
+++ src/components/AddInputsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { addInput } from "../actions/inputs";
@@ -12,6 +12,8 @@ interface UseFormInputs {
   where: string;
 }
 
+type useFormKeys = keyof UseFormInputs;
+
 export const AddInputPage = () => {
   const { register, handleSubmit, reset, errors } = useForm<UseFormInputs>();
   const onSubmit = (data: InputState) => {
@@ -19,31 +21,24 @@ export const AddInputPage = () => {
     history.push("/result");
   };
 
-  // const [inputValue, setInputValue] = useState<InputState>({
-  //   who: "",
-  //   what: "",
-  //   when: "",
-  //   where: "",
-  // });
+  const { t, i18n } = useTranslation();
 
-  // const [initializedInput, setInitializedInput] = useState({
-  //   who: false,
-  //   when: false,
-  //   where: false,
-  //   what: false,
-  // });
+  const fields: useFormKeys[] = ["who", "what", "when", "where"];
 
-  // const changeInputValue = (
-  //   inputName: string,
-  //   e: ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   setInputValue({ ...inputValue, [inputName]: e.target.value });
-  //   setInitializedInput({ ...initializedInput, [inputName]: true });
-  // };
+  const [initializedInput, setInitializedInput] = useState({
+    who: false,
+    when: false,
+    where: false,
+    what: false,
+  });
+
+  const changeInputValue = (inputName: string) => {
+    setInitializedInput({ ...initializedInput, [inputName]: true });
+  };
 
   const result = useSelector((state: InputState) => state);
 
-  useEffect = () => {
+  useEffect(() => {
     if (result) {
       setInitializedInput({
         who: true,
@@ -52,101 +47,43 @@ export const AddInputPage = () => {
         what: true,
       });
       reset(result);
-      // setInputValue({
-      //   who,
-      //   when,
-      //   where,
-      //   what,
-      // });
     }
   }, []);
 
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // const submitForm = (e: any) => {
-  //   e.preventDefault();
-  //   dispatch(addInput(inputValue));
-  //   history.push("/result");
-  // };
-
   return (
     <div className="main-container">
       <form onSubmit={handleSubmit(onSubmit)} className="input-form">
         <h1>Sentence Game</h1>
+        <h1>{t("Welcome to React")} </h1>
 
-        <div className="form-group">
-          <input
-            type="text"
-            name="who"
-            ref={register}
-            className="form-control"
-            id="formGroupExampleInput"
-            placeholder="Who?"
-            // value={inputValue.who}
-            // onChange={changeInputValue.bind(null, "who")}
-          />
-        </div>
-        <div
-          // className={initializedInput.who ? "form-group" : "form-group hidden"}
-        >
-          <input
-            type="text"
-            name="what"
-            ref={register}
-            className="form-control"
-            id="formGroupExampleInput2"
-            placeholder="What?"
-            // value={inputValue.what}
-            // onChange={changeInputValue.bind(null, "what")}
-          />
-        </div>
-        <div
-          // className={initializedInput.what ? "form-group" : "form-group hidden"}
-        >
-          <input
-            type="text"
-            name="when"
-            ref={register}
-            className="form-control"
-            id="formGroupExampleInput3"
-            placeholder="When?"
-            // value={inputValue.when}
-            // onChange={changeInputValue.bind(null, "when")}
-          />
-        </div>
-        <div
-          // className={initializedInput.when ? "form-group" : "form-group hidden"}
-        >
-          <input
-            type="text"
-            name="where"
-            ref={register}
-            className="form-control"
-            id="formGroupExampleInput4"
-            placeholder="Where?"
-            // value={inputValue.where}
-            // onChange={changeInputValue.bind(null, "where")}
-          />
-        </div>
-        <input
+        {fields.map((el) => (
+          <div className="form-group">
+            <input
+              type="text"
+              name={el}
+              ref={register({ required: true })}
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder={`${el.replace(el[0], el.charAt(0).toUpperCase())}?`}
+              onChange={changeInputValue.bind(null, el)}
+            />
+            {errors[el] && <p>Your input is required</p>}
+          </div>
+        ))}
+
+        <button
           type="submit"
-          // hidden={
-          //   !initializedInput.who ||
-          //   !initializedInput.what ||
-          //   !initializedInput.when ||
-          //   !initializedInput.where
-          //     ? true
-          //     : false
-          // }
-          // disabled={
-          //   !inputValue.who ||
-          //   !inputValue.what ||
-          //   !inputValue.when ||
-          //   !inputValue.where
-          //     ? true
-          //     : false
-          // }
+          hidden={
+            !initializedInput.who ||
+            !initializedInput.what ||
+            !initializedInput.when ||
+            !initializedInput.where
+              ? true
+              : false
+          }
           className="btn btn-primary input"
         >
           Generate a sentence!
